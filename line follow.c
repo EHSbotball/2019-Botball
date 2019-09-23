e848722b748661063cc1d935829c8ed8f0280dda
EHSbotball <48069769+EHSbotball@users.noreply.github.com>
2019-09-23 14:58:37 -0500

    Update line follow.c

--- line follow.c
+++ line follow.c
@@ -2,16 +2,14 @@ int main()
 {
 	while(1)
 	{
-		if(analog(0) > 900)
+		if(analog10(0) > 900)
 		{
 			motor(1,50);
-			motor(2,25);
 			msleep(200);
 			
 		}
 		else
 		{
-			motor(1,25);
 			motor(2,50);
 			msleep(200);
 			
